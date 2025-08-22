f64ebfcef6979a88314ddd25cbe71f46f0738195
Dinmukhammed <dinmukhammedeleu@gmail.com>
2025-08-22 22:55:04 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/*localStorage.setItem('name', 'Alice');  // сохраняем строку
+localStorage.setItem('age', 25);        // сохраняем число
+localStorage.setItem('isLoggedIn', true); // сохраняем булево значение
+
+const name = localStorage.getItem('name');
+const age = localStorage.getItem('age');
+const isLoggedIn = localStorage.getItem('isLoggedIn');*/
+
+
+const user = { name: 'Alice', age: 25, isLoggedIn: true };
+const userJSON = JSON.stringify(user); // преобразуем объект в строку
+localStorage.setItem('user', userJSON); // сохраняем строку
+
+const storedUser = localStorage.getItem('user');
+const parsedUser = JSON.parse(storedUser); // преобразуем строку обратно в объект
