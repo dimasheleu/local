b22d59efa10aa8021cda529c083e56208b51739d
Dinmukhammed <dinmukhammedeleu@gmail.com>
2025-08-25 23:45:20 +0500

    Описание ваших изменений

--- script.js
+++ script.js
@@ -29,3 +29,51 @@ const storedUserJSON = localStorage.getItem('user');
 const parsedUser1 = JSON.parse(storedUserJSON);
 console.log(parsedUser.name);
 console.log(parsedUser.preferences.theme);
+
+const user1777 = {
+  name: 'Alice',
+  age: 25,
+  isLoggedIn: true,
+  preferences: {
+    theme: 'dark',
+    language: 'en'
+  }
+};
+const userJSON177 = JSON.stringify(user);
+localStorage.setItem('user', userJSON);
+const storedUserJSON76 = localStorage.getItem('user');
+const parsedUser71 = JSON.parse(storedUserJSON);
+console.log(parsedUser.name);
+console.log(parsedUser.preferences.theme);
+
+const user178 = {
+  name: 'Alice',
+  age: 25,
+  isLoggedIn: true,
+  preferences: {
+    theme: 'dark',
+    language: 'en'
+  }
+};
+const userJSON17 = JSON.stringify(user);
+localStorage.setItem('user', userJSON);
+const storedUserJSON7 = localStorage.getItem('user');
+const parsedUser17 = JSON.parse(storedUserJSON);
+console.log(parsedUser.name);
+console.log(parsedUser.preferences.theme);
+
+const user17 = {
+  name: 'Alice',
+  age: 25,
+  isLoggedIn: true,
+  preferences: {
+    theme: 'dark',
+    language: 'en'
+  }
+};
+const userJSON12 = JSON.stringify(user);
+localStorage.setItem('user', userJSON);
+const storedUserJSON2 = localStorage.getItem('user');
+const parsedUser12 = JSON.parse(storedUserJSON);
+console.log(parsedUser.name);
+console.log(parsedUser.preferences.theme);
