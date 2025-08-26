552ef0135a718ebb18ba09daddb71a6b4c6f8677
Dinmukhammed <dinmukhammedeleu@gmail.com>
2025-08-26 23:43:04 +0500

    Описание ваших изменений

--- script.js
+++ script.js
@@ -1,79 +1,32 @@
-/*localStorage.setItem('name', 'Alice');  // сохраняем строку
-localStorage.setItem('age', 25);        // сохраняем число
-localStorage.setItem('isLoggedIn', true); // сохраняем булево значение
+//1-задание
+localStorage.setItem('greeting', 'Привет, мир!');
+const storedGreeting = localStorage.getItem('greeting');
+// console.log(storedGreeting);
+//2-задание
+const deletedGreeting = localStorage.removeItem('greeting');
+console.log(deletedGreeting);
 
-const name = localStorage.getItem('name');
-const age = localStorage.getItem('age');
-const isLoggedIn = localStorage.getItem('isLoggedIn');*/
 
+//3-задание
+const user = {
+  name: 'Dimash',
+  age: 18,
+  student: 'ENUcollege',
+  country: 
 
-const user = { name: 'Alice', age: 25, isLoggedIn: true };
-const userJSON = JSON.stringify(user); // преобразуем объект в строку
-localStorage.setItem('user', userJSON); // сохраняем строку
-
-const storedUser = localStorage.getItem('user');
-const parsedUser = JSON.parse(storedUser); // преобразуем строку обратно в объект
-
-const user1 = {
-  name: 'Alice',
-  age: 25,
-  isLoggedIn: true,
-  preferences: {
-    theme: 'dark',
-    language: 'en'
-  }
 };
-const userJSON1 = JSON.stringify(user);
+const userJSON = JSON.stringify(user);
 localStorage.setItem('user', userJSON);
 const storedUserJSON = localStorage.getItem('user');
-const parsedUser1 = JSON.parse(storedUserJSON);
-console.log(parsedUser.name);
-console.log(parsedUser.preferences.theme);
+const parsedUser = JSON.parse(storedUserJSON);
+console.log(parsedUser);
 
-const user1777 = {
-  name: 'Alice',
-  age: 25,
-  isLoggedIn: true,
-  preferences: {
-    theme: 'dark',
-    language: 'en'
-  }
-};
-const userJSON177 = JSON.stringify(user);
-localStorage.setItem('user', userJSON);
-const storedUserJSON76 = localStorage.getItem('user');
-const parsedUser71 = JSON.parse(storedUserJSON);
-console.log(parsedUser.name);
-console.log(parsedUser.preferences.theme);
 
-const user178 = {
-  name: 'Alice',
-  age: 25,
-  isLoggedIn: true,
-  preferences: {
-    theme: 'dark',
-    language: 'en'
-  }
-};
-const userJSON17 = JSON.stringify(user);
-localStorage.setItem('user', userJSON);
-const storedUserJSON7 = localStorage.getItem('user');
-const parsedUser17 = JSON.parse(storedUserJSON);
-console.log(parsedUser.name);
-console.log(parsedUser.preferences.theme);
 
-const user17 = {
-  name: 'Alice',
-  age: 25,
-  isLoggedIn: true,
-  preferences: {
-    theme: 'dark',
-    language: 'en'
-  }
-};
-const userJSON12 = JSON.stringify(user);
-localStorage.setItem('user', userJSON);
-const storedUserJSON2 = localStorage.getItem('user');
-const parsedUser12 = JSON.parse(storedUserJSON);
-console.log(parsedUser.name);
-console.log(parsedUser.preferences.theme);
+
+
+//4-задание
+//5-задание
+//6-задание
+//7-задание
+//8-задание
